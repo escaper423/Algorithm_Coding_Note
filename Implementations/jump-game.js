17e4de49c56d64c99cc6a49cc642c66df8678a8b
escaper423 <41889090+escaper423@users.noreply.github.com>
2020-05-06 16:57:47 +0900

    Solved: Jump Game(from leetcode)

---
+++ Implementations/jump-game.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+ //Problem URL: https://leetcode.com/problems/jump-game/
+function helper(nums, targetIdx){
+    if (targetIdx == 0)
+        return true;
+    
+    for(let i = targetIdx - 1; i >= 0; i--){
+        let canLeach = i + nums[i];
+        if (canLeach >= targetIdx){
+            return helper(nums,i);
+        }
+    }
+    return false;
+};
+
+
+var canJump = function(nums) {
+    var targetIdxNum= nums.length - 1;
+    return helper(nums,targetIdxNum);
+};

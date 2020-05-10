d48b0713bc5f7da4c3c6ded873caa91b021b68a1
escaper423 <escaper423@gmail.com>
2020-05-10 21:39:41 +0900

    Solved: sum_root_to_leaf_numbers (from leetcode)

---
+++ Implementations/sum_root_to_leaf_numbers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+//problem URL : https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int res = 0; 
+
+void dfs(TreeNode* root, string sum, int height) {
+	sum += to_string(root->val);
+	if (root->left == nullptr && root->right == nullptr) {
+		res += stoi(sum);
+		return;
+	}
+	if (root->left) {
+		dfs(root->left, sum, height + 1);
+	}
+	if (root->right) {
+		dfs(root->right, sum, height + 1);
+	}
+}
+
+int sumNumbers(TreeNode* root) {
+	if (root == nullptr)
+		return 0;
+	dfs(root, "", 0);
+	return res;
+}
+
+int main() {
+	TreeNode* root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+	cout << sumNumbers(root);
+	return 0;
+}

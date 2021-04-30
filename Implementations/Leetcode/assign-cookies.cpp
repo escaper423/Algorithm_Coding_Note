41b0bb907a4d7f4f83a87812f6818667434e4356
escaper423 <41889090+escaper423@users.noreply.github.com>
2021-04-30 19:02:58 +0900

    Create assign-cookies.cpp

---
+++ Implementations/Leetcode/assign-cookies.cpp
@@ -0,0 +1,20 @@
+//problem URL: https://leetcode.com/problems/assign-cookies/
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        int p1 = 0; int p2 = 0;
+        while(p1 < g.size() && p2 < s.size()){
+            if (g[p1] > s[p2]){
+                p2++;
+            }
+            else if (g[p1] <= s[p2]){
+                p1++;
+                p2++;
+            }
+        }
+        return p1;
+    }
+};

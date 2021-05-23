a6599fa7a30e94a9b89d0e97e08eae56e132032b
escaper423 <41889090+escaper423@users.noreply.github.com>
2021-05-23 22:03:56 +0900

    Create n-ary-tree-postorder-traversal.cpp

---
+++ Implementations/Leetcode/n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+//problem URL: https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+class Solution {
+private:
+    vector<int> res;
+    
+public:
+    void solve(Node* root){
+        if(!root){
+            return;
+        }
+        for(auto &it : root->children){
+            solve(it);
+        }
+        res.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+         solve(root);
+        return res;
+        
+    }
+};
